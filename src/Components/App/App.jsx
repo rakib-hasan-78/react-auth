b9d92e5d50f078df469c036c926bba7c9801cb0a
rakib hasan <rakibwebdev78@gmail.com>
2026-01-29 17:36:53 +0600

    auth practice....

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+
+import '../../App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>react authentication</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2JHyue50yGAxw0qbMXQQAh-vTUsvaSm0",
+  authDomain: "react-auth-8a9a7.firebaseapp.com",
+  projectId: "react-auth-8a9a7",
+  storageBucket: "react-auth-8a9a7.firebasestorage.app",
+  messagingSenderId: "173189009271",
+  appId: "1:173189009271:web:7569e4ef42b5112cf71b72"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
