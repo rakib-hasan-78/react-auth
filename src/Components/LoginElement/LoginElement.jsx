4c7b0f97e9d4d7582dc66219911ea8d96f18402b
rakib hasan <rakibwebdev78@gmail.com>
2026-02-07 04:51:58 +0600

    log out done...

--- src/Components/LoginElement/LoginElement.jsx
+++ src/Components/LoginElement/LoginElement.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import { googleLoginHandler } from '../../handlers/handlers';
+import React, { useState } from 'react';
+import { googleLoginHandler, signoutHandler } from '../../handlers/handlers';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(()=>{
+        const loginUser = onAuthStateChanged(auth, (loginUser)=>{
+            setUser(loginUser);
+        });
+
+        return ()=> loginUser();
+        
+    },[]);
+
     return (
         <div>
-            
-            <button
-            onClick={googleLoginHandler}
-             style={{margin:'10px'}}
-              type="button"
-              >
-              sign with Google
-              </button>
+            {
+                user ? 
+                <div>
+                    <p>{user?.email}</p>
+                    <img
+                     style={{ width: '100px', height: '100px', borderRadius: '50%' }}
+                    src={user?.photoURL} 
+                    alt={user?.displayName}
+                     />
+                     <p>{user?.displayName}</p>
+
+                     <button
+                     onClick={signoutHandler}
+                      type="button"
+                      >
+                      log out
+                      </button>
+
+                </div> : 
+                <div>
+                    <button
+                    onClick={googleLoginHandler}
+                    style={{margin:'10px'}}
+                    type="button"
+                    >
+                    sign with Google
+                    </button>
+
+                </div>
+            }
         </div>
     );
 };
--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -1,10 +1,8 @@
 
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../firebase/firebase.init";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from './../firebase/firebase.init';
 
 const provider = new GoogleAuthProvider();
-
-
 
 export const googleLoginHandler=()=>{
         signInWithPopup(auth, provider)
@@ -13,4 +11,12 @@ export const googleLoginHandler=()=>{
     }).catch((error) => {
         console.log(error);
     });
+}
+
+export const signoutHandler = ()=>{
+    signOut(auth).then(()=>{
+
+    }).catch((error)=>{
+        
+    })
 }
